1286e68672e5b592c22b8b49d10b03c6696b3a63
rnutall <rnutall1@gmail.com>
2023-03-04 01:18:34 -0600

    update to js card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
-$(button).on("#card3") {
-    localStorage.setItem("card3");
-}
+//button in jquery W/ local storage
+$(button).on("#card3"); {
+    var babyName = ("")
+    localStorage.setItem("card3")
+    localStorage.removeItem("card3")
+};
