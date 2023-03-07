c833aec22919c4fe6dd3b0f682b6356772292c80
palejrey@gmail.com <palejrey@gmail.com>
2023-03-06 20:38:03 -0600

    Import JQuery library and add functionality to associated buttons with associated javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
-//button in jquery W/ local storage
-$(button).on("#card3"); {
-    var babyName = ("")
-    localStorage.setItem("card3")
-    localStorage.removeItem("card3")
-    addEventListener("#card3")
-};
+var dynamic_content = document.getElementById("dynamic_content")
+
+$("#get-name-information").on("click",function(){
+    inputValue = $("#baby_name_input").val()
+    console.log(inputValue)
+    fetch("https://www.behindthename.com/api/lookup.json?name=" + inputValue+"&key=re323908171").then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    });
+    
+});
