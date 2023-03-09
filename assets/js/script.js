bc20e3e054daf9d1677856036d2f6852f4c24fb5
xybai0103 <xueyin0103@gmail.com>
2023-03-08 20:09:59 -0600

    based on the structured displayed in the wireframe, create tags in html for the dynamic content of name-generating box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,35 @@
-/* Initialize variables */
-// an acquired API key to use babyName API
-var APIKey = 'jmjo9cnRKX3lkeCzzsTXtA==f4OpgctSgGbxedYt';
-// button to generate baby names
-var generateBtn = $('.generate-button');
+// /* Initialize variables */
+// // an acquired API key to use babyName API
+// var APIKey = 'jmjo9cnRKX3lkeCzzsTXtA==f4OpgctSgGbxedYt';
+// // button to generate baby names
+// var generateBtn = $('.generate-button');
+// // the container of all the synamic content for name-generating box
+// var generatedNameContainer = $('.generated-name-container');
+// // the header above the generated baby names
+// var generatedNnameHeader = $('.generated-name-header');
+// // the text showing gender in the generated-name-header
+// var genderText = $('.gender-text');
+// // a list containing the 10 randomly generated baby names
+// var generatedNameList = $('.generated-name-list');
 
 
 /* Define functions */
-function callGenerateAPI() {
+function generateBabyNames() {
+  // user's input for the dropdown menu and the checkbox
   var gender = $('#gender-category').val();
   var popular = $('.checkbox').prop('checked');
+  // // show different texts corresponding to user's input
+  // genderText.text(gender + ' ');
+  // if(popular){
+  //   // create the text showing popularity in the generated-name-header
+  //   var popularText = $('<span>');
+  //   popularText.addClass('popular-text')
+  //   popularText.text(' (Top 10% Most Popular)')
+  //   generatedNnameHeader.append(popularText);
+  // };
+  // // fix bug above!!
+
+  // the constructed URL based on user's input
   var babyNameGenerateURL = 'https://api.api-ninjas.com/v1/babynames?gender=' + gender + '&popular_only=' + popular;
   // call the API with the constructed URL
   fetch(babyNameGenerateURL,{
@@ -16,21 +37,38 @@ function callGenerateAPI() {
       'X-Api-Key': APIKey
     }  
   })
-  // .then(function(response){
-  //     return response.json();
-  //   })
-  //   .then(function(data){
-  //     console.log(data);
-  //   })
-    .then(response => response.json())
-    .then(data => console.log(data))
+  .then(function(response){
+      return response.json();
+    })
+    .then(function(data){
+      console.log(data);
+      // // loop through the name array and generate list to display the names
+      // for(i=0; i<data.length; i++){
+      //   // create a list item tag for each generated name
+      //   var generatedName = $('<li>');
+      //   generatedName.addClass('generated-name')
+      //   generatedName.text(data[i]);
+      //   // wire up an add-name button after each name
+      //   var addNameBtn =$('<button>');
+      //   addNameBtn.addClass('add-name-button is-size-5');
+      //   addNameBtn.text('Add ')
+      //   // add a plus symbol for the add-name button
+      //   var plusSymbol = $('<i>');
+      //   plusSymbol.addClass('plus-symbol fa fa-plus');
+      //   addNameBtn.append(plusSymbol);
+      //   generatedName.append(addNameBtn);
+      //   generatedNameList.append(generatedName);
+      // };
+    })
  }
 
 
  /*Add Event Listeners*/
- generateBtn.click(callGenerateAPI);
-var dynamic_content = document.getElementById("dynamic_content")
+ generateBtn.click(generateBabyNames);
 
+
+
+var dynamic_content = document.getElementById("dynamic_content")
 $("#get-name-information").on("click",function(){
     inputValue = $("#baby_name_input").val()
     console.log(inputValue)
